a2c1ffedf513ac576a152adc1439ae59c82483b7
padmashree <padmashreeshetty245@gmail.com>
2022-05-27 21:27:19 +0530

    extended the height

--- index.php
+++ index.php
@@ -18,27 +18,27 @@
 
 
     <h1 class=" bg-[rgb(91,222,26)] text-center text-2xl font-bold  tracking-wide">SCHOOL MANAGEMENT SYSTEM</h1>
-    <div class="flex">
+    <div class="flex h-[700px]">
         <div class=" bg-[rgb(68,241,105)] text-center p-[1vw]  ">
             
             <form action="index.php" method="post">
-              <label for="name"> Name </label>
+              <label for="name"> Name </label><br>
              <input type="text" name="name" id="name"><br>
-             <label for="number">  Contact Number </label>
-            <input type="number" name="contact" id="number">
-              <label for="emailadd"> Email Address</label>
-              <input type="email" name="email" id="emailadd">
-              <label for="gen"> Gender</label>
-              <select name="gender" id="gen">
+             <label for="number">  Contact Number </label><br>
+            <input type="number" name="contact" id="number"><br>
+              <label for="emailadd"> Email Address</label><br>
+              <input type="email" name="email" id="emailadd"><br>
+              <label for="gen"> Gender</label><br>
+              <select name="gender" id="gen"><br>
                   <option value="male">Male</option>
                   <option value="female">Female</option>
-              </select>
-              <label for="dob">Date of Birth </label>
-              <input type="date" name="dateofbirth" id="dob">
-              <label for="str">   Stream </label>
+              </select><br>
+              <label for="dob">Date of Birth </label><br>
+              <input type="date" name="dateofbirth" id="dob"><br>
+              <label for="str">   Stream </label><br>
            <input type="text" name="stream" id="str"><br>
               <button type="submit" name="submit" class="bg-white p-1  ">Submit and Add Record</button>
-            </form>
+            </form><br>
         </div>
         <div class=" bg-[rgb(195,242,107)] flex flex-col  justify-center  p-2">
             <form method='post' >
